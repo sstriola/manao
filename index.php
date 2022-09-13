ff27ac525d2d6c6eae66ebfa782c76b446b6c4b8
Egor <sentriola@gmail.com>
2022-09-13 23:12:00 +0300

    initial commit

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Manao Authorization</title>
+  <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-
+  <form class="" action="" method="">
+    <label>Login</label>
+    <input type="Text" placeholder="Enter your login">
+    <label>Password</label>
+    <input type="Text" placeholder="Enter your password">
+    <button>Visit</button>
+    <p>You dont have an account? <a href="register.php">Register Now!</a></p>
+  </form>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Manao Registration</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<form class="" action="" method="">
+	<label>Name</label>
+	<input type="Text" placeholder="Enter your Name">
+  <label>Email</label>
+  <input type="Email" placeholder="Enter your Email">
+  <label>Login</label>
+  <input type="Text" placeholder="Enter your Login">
+  <label>Password</label>
+  <input type="Text" placeholder="Enter your password">
+  <label>Confirm Password</label>
+  <input type="Text" placeholder="Confirm your password">
+  <button>Register</button>
+	<p>Already have an account? <a href="index.php">Log In</a></p>
+</form>
+</body>
+</html>
